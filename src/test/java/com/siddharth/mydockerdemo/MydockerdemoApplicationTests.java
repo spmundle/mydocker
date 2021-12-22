b29c307a09ed1c798d1681c58eb0b73eddedfa75
spmundle <mundle29siddharth@gmail.com>
2021-12-22 13:11:41 +0530

    First project setup

---
+++ src/test/java/com/siddharth/mydockerdemo/MydockerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddharth.mydockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydockerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
